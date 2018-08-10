bbfd954a98cb0090557969373be5ad2482f83f73
July Atauje <jatauje@gmail.com>
2018-08-10 15:28:26 -0500

    seccion 5

---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,11 @@
+
+export interface InfoPagina {
+  titulo ?: string;
+  email ?: string;
+  nombre_corto ?: string;
+  equipo_de_trabajo ?: any[];
+  facebook ?: string;
+  twitter ?: string;
+  instagram ?: string;
+  tumblr ?: string;
+}
