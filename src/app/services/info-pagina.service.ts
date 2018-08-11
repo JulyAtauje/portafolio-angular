88794c6e221a492f5a07f2a88b51db190245fca2
July Atauje <jatauje@gmail.com>
2018-08-10 20:26:06 -0500

    seccion 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -31,7 +31,6 @@ export class InfoPaginaService {
     this.http.get('https://angular-html-a9122.firebaseio.com/equipo.json')
     .subscribe( (resp: any[]) => {
       this.equipo = resp;
-      console.log(resp);
     });
   }
 
